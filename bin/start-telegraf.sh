ce5199defec5baf022f705cb4dfb3b9621cbd7f6
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-05 17:49:58 -0400

    Add start telegraf script.

---
+++ bin/start-telegraf.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+export INFLUX_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_influxdb_1`
+export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_nats_1`
+export PG_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_db_1`
+export NGINX_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_reverse-proxy_1`
+
+sudo service telegraf start
