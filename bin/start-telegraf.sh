698d1fe6bb36e5a984950cec79042ddba3420368
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-05 17:50:12 -0400

    Remove sudo usage.

--- bin/start-telegraf.sh
+++ bin/start-telegraf.sh
@@ -5,4 +5,4 @@ export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAdd
 export PG_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_db_1`
 export NGINX_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_reverse-proxy_1`
 
-sudo service telegraf start
+service telegraf start
