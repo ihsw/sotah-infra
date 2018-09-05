072aadd3be5587c324a07670e4bd7c95038778f0
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-05 19:32:02 -0400

    Rename file for clarity.

--- bin/export-telegraf-env-vars.sh
+++ bin/export-telegraf-env-vars.sh
@@ -4,5 +4,3 @@ export INFLUX_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPA
 export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_nats_1`
 export PG_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_db_1`
 export NGINX_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_reverse-proxy_1`
-
-service telegraf start
