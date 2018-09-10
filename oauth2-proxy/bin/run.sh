7db89c3139d0494bfbf426dd7708d78d940696d2
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-09 21:58:07 -0400

    Add missing param.

--- oauth2-proxy/bin/run.sh
+++ oauth2-proxy/bin/run.sh
@@ -5,4 +5,5 @@
     -upstream=http://$KIBANA_HOST:5601 \
     -email-domain=$GOOGLE_DOMAINS \
     -redirect-url=https://kibana.sotah.info/oauth2/callback \
-    -cookie-secret=$OAUTH2_PROXY_COOKIE_SECRET
+    -cookie-secret=$OAUTH2_PROXY_COOKIE_SECRET \
+    -cookie-secure=false
