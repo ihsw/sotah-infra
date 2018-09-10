813373d1302aaee2a38c4d1cd5b8e675f3b469e9
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-09 21:52:15 -0400

    Add missing redirect-url config.

--- oauth2-proxy/bin/run.sh
+++ oauth2-proxy/bin/run.sh
@@ -3,4 +3,5 @@
 ./oauth2_proxy \
     -http-address=http://0.0.0.0:4180 \
     -upstream=http://$KIBANA_HOST:5601 \
-    -email-domain=$GOOGLE_DOMAINS
+    -email-domain=$GOOGLE_DOMAINS \
+    -redirect-url=https://kibana.sotah.info/oauth2/callback
